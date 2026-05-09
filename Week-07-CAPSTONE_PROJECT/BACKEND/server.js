b51b597ab088c68f403fdd3096686b428f96d4df
naveenkashaboina <naveenkashaboina10@gmail.com>
2026-05-09 12:36:58 +0530

    Updated CORS origin to allow Vercel frontend

--- Week-07-CAPSTONE_PROJECT/BACKEND/server.js
+++ Week-07-CAPSTONE_PROJECT/BACKEND/server.js
@@ -15,7 +15,7 @@ config();
 const app = exp();
 //enable cors
 app.use(cors({
-  origin:['http://localhost:5173'],
+  origin:['blog-frontend-alpha-lovat.vercel.app'],
   credentials:true
 }))
 //add cookie parser middeleware
