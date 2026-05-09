7747cf43ee56811a30a6b45ef459d17c47a2835c
naveenkashaboina <naveenkashaboina10@gmail.com>
2026-05-09 10:59:34 +0530

    updated case‑sensitivity for deployment
    Refactor server.js to improve logging and formatting.

--- Week-07-CAPSTONE_PROJECT/BACKEND/server.js
+++ Week-07-CAPSTONE_PROJECT/BACKEND/server.js
@@ -32,12 +32,18 @@ app.use("/auth", commonApp);
 const connectDB = async () => {
   try {
     await connect(process.env.DB_URL);
+
     console.log("DB server connected");
-    //assign port
+
     const port = process.env.PORT || 5000;
-    app.listen(port, () => console.log(`server listening on ${port}..`));
+
+    app.listen(port, () =>
+      console.log(`server listening on ${port}..`)
+    );
+
   } catch (err) {
-    console.log("err in db connect", err);
+    console.log("err in db connect", err.message);
+    process.exit(1);
   }
 };
 
